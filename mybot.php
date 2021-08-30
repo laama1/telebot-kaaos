5086e987ae35078a435fda416cce92a147e6ca73
centrifug <centrifug@gmail.com>
2021-08-30 19:42:22 +0300

    new feature: nytsoivideo

--- mybot.php
+++ mybot.php
@@ -24,7 +24,7 @@ class MyBot {
 	private $channels = array();	// telegram channels / their id's
 	private $chatId = '';			// where the message came from
 	private $tags = '';				// id-tags for stream
-	private $listenurl = 'http://kaaosradio.fi:8000/';		// kaaosradio icecast server address
+	private $listenurl = 'https://kaaosradio.fi:8001/';		// kaaosradio icecast server address
 
 	public function __construct() {
 		$this->logfile = dirname(__FILE__). '/logs/debug.txt';
@@ -38,23 +38,27 @@ class MyBot {
 		$this->logenabled = $logenabled;
 		$tags = '';
 		$data = '';
-
 		if (isset($_GET) && isset($_GET['nytsoi'])) {
-			$data = $_GET['nytsoi'];
-			$this->msg_to_kaaos('<b>Nytsoi päivitetty!</b> '.$data. ' http://stream.kaaosradio.fi:8000/stream');
+			$data = utf8_decode($_GET['nytsoi']);
 			$this->log(__LINE__.' Nytsoi: '.$data);
+			$this->msg_to_kaaos('<b>Nytsoi päivitetty!</b> '.$data. ' https://stream.kaaosradio.fi:8001/stream');
 			return;
 		} elseif (isset($_GET) && isset($_GET['viesti'])) {
 			$data = $_GET['viesti'];
 			$this->log(__LINE__.' Viesti: '.$data);
 			//$this->msg_to_bot_test(utf8_decode($data));
-			$this->msg_to_kaaos(utf8_decode($data));  
+			$this->msg_to_kaaos(utf8_decode($data));
+			return;
+		} elseif (isset($_GET) && isset($_GET['nytsoivideo'])) {
+			$data = $_GET['nytsoivideo'];
+			$this->log(__LINE__.' Nytsoivideo: '.$data);
+			$this->msg_to_kaaos('<b>Videostream!</b> '.$data. ' https://videostream.kaaosradio.fi');
 			return;
 		}
 		if (isset($update['message'])) {
 			// Private message
-			$this->chatId = $update["message"]["chat"]["id"];
 			$data = $update["message"]["text"];
+			$this->chatId = $update["message"]["chat"]["id"];
 		} elseif (isset($update['channel_post'])) {
 			// Channel message
 			$data = $update['channel_post']['text'];
@@ -62,29 +66,29 @@ class MyBot {
 		}
 		if ($data = '') return;
 
-		if (strpos($data, "!np stream2") === 0) {
+		if (strpos($data, "!np stream2") !== false) {
 			$tags = file_get_contents('http://kaaosradio.fi/npfile_stream2_tags');
 			$tags .= "\n".$this->listenurl.'stream2';
 
-		} elseif (strpos($data, "!np chill") === 0) {
+		} elseif (strpos($data, "!np chill") !== false) {
 			$tags = "<b>np:</b> ".file_get_contents('http://kaaosradio.fi/npfile_chill_tags');
 			$tags .= "\n".$this->listenurl.'chill';
 
-		} elseif (strpos($data, "!np chip") === 0) {
+		} elseif (strpos($data, "!np chip") !== false) {
 			$tags = "<b>np:</b> ".file_get_contents('http://kaaosradio.fi/npfile_chip_tags');
 			$tags .= "\n".$this->listenurl.'chip';
 
-		} elseif (strpos($data, "!np") === 0) {
+		} elseif (strpos($data, "!np") !== false) {
 			$tags = "<b>np:</b> ".file_get_contents('http://kaaosradio.fi/npfile_stream2_tags');
 			//$tags .= "<br>".$this->listenurl.'stream2';
 
-		} elseif (strpos($data, "!nytsoi") === 0) {
+		} elseif (strpos($data, "!nytsoi") !== false) {
 			$tags = "<b>Nytsoi:</b> ".file_get_contents('http://kaaosradio.fi/nytsoi.txt');
 			
-		} elseif (strpos($data, '!kuuntelijat') === 0 || strpos($data, '!kuulijat') === 0) {
+		} elseif (strpos($data, '!kuuntelijat') !== false || strpos($data, '!kuulijat') !== false) {
 			$this->get_kuuntelijat();
 			return;
-		} elseif (strpos($data, '!s') === 0) {
+		} elseif (strpos($data, '!s') !== false) {
 			$w = new Weather($data);
 			$tags = $w->getMessage();
 		}
@@ -103,9 +107,9 @@ class MyBot {
 		$chat = urlencode($text);
 		//$this->log(__LINE__.' chat: '.$chat);
 		$url = $this->path.'/sendmessage?chat_id='.$this->channels['kaaosradio'].'&parse_mode=html&text='.$chat;
-		$this->log(__LINE__.' url: '.$url);
+		//$this->log(__LINE__.' url: '.$url);
 		$response = file_get_contents($url);
-		$this->log(__LINE__.' response: ');
+		$this->log(__LINE__.' response (in next line): ');
 		$this->log($response);
 	}
 
@@ -121,13 +125,24 @@ class MyBot {
 
 	private function log($text) {
 		if ($this->logenabled) {
-			file_put_contents($this->logfile, date('Y-m-d H:i:s').': '.$text . PHP_EOL, FILE_APPEND);
+			file_put_contents($this->logfile, date('Y-m-d H:i:s').': IP: '.$this->get_ip().', '.$text . PHP_EOL, FILE_APPEND);
 		}
 	}
 
 	private function get_kuuntelijat() {
 		$data = file_get_contents('http://kaaosradio.fi/kuuntelijat/kuuntelijat_api.php');
 		$this->msg_to_kaaos($data);
+	}
+
+	private function get_ip() : string {
+		if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))	{
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
 	}
 }
 
