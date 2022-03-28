ea724dd622b2199dc61db03726dacca137d85ff5
centrifug <14946062+laama1@users.noreply.github.com>
2022-03-28 17:36:28 +0300

    bugfix

--- plugins/np.php
+++ plugins/np.php
@@ -1,21 +1,21 @@
 <?php
 
 class Np {
-    private $npfile_address = 'http://kaaosradio.fi/npfile_';
+    private $npfile_address = 'https://kaaosradio.fi/npfile_';
     private $listen_address = 'https://kaaosradio.fi:8001/';
 
     public function __construct() {
 
     }
-    public function handle($args) : string {
+    public function handle($args) : ?string {
         $tags = '';
-        if ($args[1] == '') {
-            $args[1] = 'stream2';
+        if (!$args[0]) {
+            $args[0] = 'stream2';
         }
-        if ($tags = file_get_contents($this->npfile_address.$args[1].'_tags')) {
-            $tags .= "\n".$this->listen_address.$args[1];
+        if ($tags = file_get_contents($this->npfile_address.$args[0].'_tags')) {
+            if ($args[0] != 'stream2') $tags .= "\n".$this->listen_address.$args[0];
+            return $tags;
         }
-        return $tags;
     }
 }
 
