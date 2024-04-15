425cfef56a08dab41dfcde87df9441e4f459aaea
centrifug <14946062+laama1@users.noreply.github.com>
2024-04-15 19:25:14 +0300

    new conf options

--- config.php
+++ config.php
@@ -1,7 +1,31 @@
 <?php
-$path = 'https://api.telegram.org/bot11111';
-$channels = array('kaaos-bot-testing' => '-1', 'kaaosradio' => '-2');
+
+# Telegram stuff
+# api url
+$path = 'https://api.telegram.org/bot11';
+# channels
+$channels = array('kaaos-bot-testing' => '-1001180164432', 'kaaosradio' => '-1001120850648');
 $logenabled = 1;
-$openwmapapikey = '4b';
-$weatherdb = 'openweathermap.db';
-?>
+
+# Weather script stuff
+$openwmapapikey = '';
+$weatherdb = '';
+
+// KAaosradio stuff
+$icecast_url = 'https://stream.kaaosradio.fi:8001/stream';
+$kaaosradio_url = 'https://kaaosradio.fi';
+$kaaosradio_video_page_url = 'https://videostream.kaaosradio.fi/';
+$icecast_url = 'https://kaaosradio.fi:8001/stream';
+$rtmp_video_statsurl = 'https://video.kaaosradio.fi:8081/stats';
+$rtmp_video_url = 'rtmp://video.kaaosradio.fi/';
+
+// discord stuff
+# announcements channel:
+$discord_webhookurl = "https://discordapp.com/api/webhooks/8...";
+# botspam channel:
+$discord_botspam_webhookurl = "https://discordapp.com/api/webhooks/83...";
+$discord_app_pub_key = 'f...';
+
+// Twitch stuff
+$twitch_client_id = 't...';
+$twitch_oauth_token = 'u...';
